62fc573a09543e43f6b8220370904c9578aa1f39
Aleksandar <aleksandermihaylov@gmail.com>
2019-04-02 10:43:28 +0300

    first commit

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <h1>{{msg}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+  data() {
+    return {
+      msg: "Hellow World"
+    };
+  }
+};
+</script>
+
+<style>
+</style>
